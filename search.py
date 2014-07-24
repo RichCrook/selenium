1f8d36add70759e0475485fcc49c588d7dc3a584
RichCrook <richcrook@yahoo.com>
2014-07-24 12:52:07 -0700

    updates

--- search.py
+++ search.py
@@ -1,21 +1,42 @@
 import unittest
 from selenium import webdriver
- # Tests with the page object pattern.
-  class SearchTest(unittest.TestCase):
-      def setUp(self):
-          self.browser = webdriver.Firefox()
-  
-      def test_someone_can_search(self):
-          homepage = Homepage(self.browser)
-          homepage.navigate()
-          search_form = homepage.getSearchForm()
-          search_form.setName("Hercules")
+from selenium.webdriver.common.keys import Keys  # The Keys class provide keys in the keyboard like RETURN, F1, ALT etc.
+from ConfigParser import SafeConfigParser
 
-          search_form.submit()
+parser = SafeConfigParser()
+parser.read('inputs.ini')
+
+
+class Search(unittest.TestCase):
+    """
+     This module search the IMDB website.  
+     To generate HTML documentation for this module issue the
+     command and regenerate this document:
  
-  
-          #elem = self.browser.find_element_by_css_selector('#logged_in_header')
-          #assert elem is not None
-  
-      def tearDown(self):
-          self.browser.close()
+         pydoc -w registration
+ 
+     """
+
+    def setUp(self):
+        """
+        Search for a movie.
+        """
+        self.driver = webdriver.Firefox()
+
+
+    def test_search(self):
+        """
+        Search Tab In test_cases.ods - Test Case #search_001
+        """
+        self.driver.get(parser.get('SectionTwo', 'url_in_test'))
+        search_box = self.driver.find_element_by_id("navbar-query").send_keys('Hercules')
+        self.driver.find_element_by_id("navbar-query").send_keys(Keys.RETURN);
+        title_result = self.driver.find_element_by_xpath(".//*[@id='main']/div/div[2]/table/tbody/tr[1]/td[2]")
+        assert title_result.text == 'Hercules (2014)'
+
+    def tearDown(self):
+        self.driver.close()  # The quit will exit entire browser, where as close will close a tab, but if it is just one tab, by default most browser will exit entirely
+
+if __name__ == "__main__":  # Final lines are some boiler plate code to run the test suite
+    unittest.main()  # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
+    # magic methods
